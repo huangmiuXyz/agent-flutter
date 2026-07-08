7f1446f3b4330a13b179ef2fb7d09815a8ab6095
huangmiu <534496845@qq.com>
2026-07-08 21:16:31 +0800

    feat: refactor TerminalWidget to use HookWidget and update dependencies

--- lib/widgets/terminal/terminal_widget.dart
+++ lib/widgets/terminal/terminal_widget.dart
@@ -1,88 +1,75 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_pty/flutter_pty.dart';
-import 'package:xterm/xterm.dart';
+import 'package:kterm/kterm.dart';
 
-class TerminalWidget extends StatefulWidget {
+class TerminalWidget extends HookWidget {
   const TerminalWidget({super.key});
 
   @override
-  State<TerminalWidget> createState() => _TerminalWidgetState();
-}
-
-class _TerminalWidgetState extends State<TerminalWidget> {
-  late final Terminal _terminal;
-  Pty? _pty;
-  StreamSubscription? _outputSub;
-
-  @override
-  void initState() {
-    super.initState();
-    _terminal = Terminal(
-      onOutput: _onOutput,
-      onResize: _onResize,
-    );
-    WidgetsBinding.instance.endOfFrame.then((_) {
-      if (mounted) _startPty();
-    });
-  }
-
-  @override
-  void dispose() {
-    _outputSub?.cancel();
-    _pty?.kill();
-    super.dispose();
-  }
-
-  void _startPty() {
-    final shell = Platform.isWindows
-        ? 'cmd.exe'
-        : (Platform.environment['SHELL'] ?? '/bin/bash');
-
-    _pty = Pty.start(
-      shell,
-      columns: _terminal.viewWidth,
-      rows: _terminal.viewHeight,
-      workingDirectory: Platform.environment['HOME'] ?? '/',
-    );
-
-    _outputSub = _pty!.output
-        .cast<List<int>>()
-        .transform(const Utf8Decoder())
-        .listen((data) => _terminal.write(data));
-
-    _pty!.exitCode.then((code) {
-      if (mounted) {
-        _terminal.write('\r\n[exit $code]\r\n');
-      }
-    });
-  }
-
-  void _onOutput(String data) {
-    _pty?.write(const Utf8Encoder().convert(data));
-  }
-
-  void _onResize(int width, int height, int pixelWidth, int pixelHeight) {
-    _pty?.resize(height, width);
-  }
-
-  void restart() {
-    _outputSub?.cancel();
-    _pty?.kill();
-    _terminal.write('\x1b[2J\x1b[H');
-    _startPty();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final ptyRef = useRef<Pty?>(null);
+    final subscriptionRef = useRef<StreamSubscription?>(null);
+    final focusNode = useFocusNode();
+    final controller = useMemoized(() => TerminalController());
+
+    final terminal = useMemoized(() {
+      final t = Terminal();
+      t.onOutput = (String data) {
+        ptyRef.value?.write(const Utf8Encoder().convert(data));
+      };
+      t.onResize = (int width, int height, int pixelWidth, int pixelHeight) {
+        ptyRef.value?.resize(height, width);
+      };
+      return t;
+    }, []);
+
+    void startPty() {
+      final shell = Platform.isWindows
+          ? 'cmd.exe'
+          : (Platform.environment['SHELL'] ?? '/bin/bash');
+
+      final pty = Pty.start(
+        shell,
+        columns: terminal.viewWidth,
+        rows: terminal.viewHeight,
+      );
+
+      ptyRef.value = pty;
+
+      final sub = pty.output
+          .cast<List<int>>()
+          .transform(const Utf8Decoder())
+          .listen((data) => terminal.write(data));
+      subscriptionRef.value = sub;
+
+      pty.exitCode.then((code) {
+        terminal.write('\r\n[exit $code]\r\n');
+      });
+    }
+
+    useEffect(() {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (context.mounted) {
+          startPty();
+          focusNode.requestFocus();
+        }
+      });
+
+      return () {
+        subscriptionRef.value?.cancel();
+        ptyRef.value?.kill();
+      };
+    }, const []);
+
     return ClipRect(
       child: TerminalView(
-        _terminal,
+        terminal,
+        controller: controller,
         autofocus: true,
-        theme: TerminalThemes.defaultTheme,
       ),
     );
   }
